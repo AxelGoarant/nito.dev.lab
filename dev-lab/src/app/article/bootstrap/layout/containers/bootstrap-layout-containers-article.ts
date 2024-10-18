2cc402c932424e6abd95c5dbceff70bc830afc9f
Axel GOARANT-VAN <Axel.Goarant@telecomnancy.eu>
2024-10-18 13:57:36 +0200

    v0.2.1 : add article versioning and verification

--- dev-lab/src/app/article/bootstrap/layout/containers/bootstrap-layout-containers-article.ts
+++ dev-lab/src/app/article/bootstrap/layout/containers/bootstrap-layout-containers-article.ts
@@ -6,40 +6,43 @@ import {ArticleMetaData} from "../../../common/article-meta-data.model";
 import {Technology} from "../../../../main/model/technology.model";
 
 const title = 'Layout : Containers';
+const version = '5.3.0';
 
 @Component({
-    selector: 'app-containers',
-    standalone: true,
-    imports: [
-        ArticleComponent,
-        CodePlaygroundComponent,
-        CodeSectionComponent,
-    ],
-    templateUrl: './bootstrap-layout-containers-article.html',
-    styleUrl: './bootstrap-layout-containers-article.css',
+  selector: 'app-containers',
+  standalone: true,
+  imports: [
+    ArticleComponent,
+    CodePlaygroundComponent,
+    CodeSectionComponent,
+  ],
+  templateUrl: './bootstrap-layout-containers-article.html',
+  styleUrl: './bootstrap-layout-containers-article.css',
 })
 export class BootstrapLayoutContainersArticle {
-    title = title;
+  title = title;
+  version = version;
 
-    code001 = "<div class=\"container\">\n" +
-        "  <!-- Content here -->\n" +
-        "</div>";
+  code001 = "<div class=\"container\">\n" +
+    "  <!-- Content here -->\n" +
+    "</div>";
 
-    code002 = "<div class=\"container-sm\">100% wide until small breakpoint</div>\n" +
-        "<div class=\"container-md\">100% wide until medium breakpoint</div>\n" +
-        "<div class=\"container-lg\">100% wide until large breakpoint</div>\n" +
-        "<div class=\"container-xl\">100% wide until extra large breakpoint</div>\n" +
-        "<div class=\"container-xxl\">100% wide until extra extra large breakpoint</div>";
+  code002 = "<div class=\"container-sm\">100% wide until small breakpoint</div>\n" +
+    "<div class=\"container-md\">100% wide until medium breakpoint</div>\n" +
+    "<div class=\"container-lg\">100% wide until large breakpoint</div>\n" +
+    "<div class=\"container-xl\">100% wide until extra large breakpoint</div>\n" +
+    "<div class=\"container-xxl\">100% wide until extra extra large breakpoint</div>";
 
-    code003 = "<div class=\"container-fluid\">\n" +
-        "  ...\n" +
-        "</div>";
+  code003 = "<div class=\"container-fluid\">\n" +
+    "  ...\n" +
+    "</div>";
 }
 
 export const BOOTSTRAP_LAYOUT_CONTAINERS_ARTICLE: ArticleMetaData<any> = {
-    component: BootstrapLayoutContainersArticle,
-    name: title,
-    keywords: [Technology.Bootstrap, "layout", "containers"],
-    technology: Technology.Bootstrap,
-    url: [Technology.Bootstrap, "layout", "containers"]
+  component: BootstrapLayoutContainersArticle,
+  name: title,
+  keywords: [Technology.Bootstrap, "layout", "containers"],
+  technology: Technology.Bootstrap,
+  url: [Technology.Bootstrap, "layout", "containers"],
+  version: version
 };
